085ac9d63afe5af4d17ee7d47628fe4b028641e7
BillBillBillBill <billo@qq.com>
2017-02-23 01:04:49 +0800

    微信推送相关

---
+++ wechat_config.py
@@ -0,0 +1,10 @@
+
+appid = 'wx98236d864d8a122a'
+appsecret = '2530005d0440a7d3980ab6abca806357'
+token_url = 'https://api.weixin.qq.com/cgi-bin/token?'
+maintainers = ['oCfHOwD2_5ZpGctshTZPeZHqUIjc']
+expires_time = None
+
+template_id = 'B1ftH9IQr4Cuy_1M_94Je851RKGgQU-Uc0CH5ej4-oo'
+send_url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='
+template_url = 'https://www.google.com.hk/search?q='
---
+++ wechat_push.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+import json
+import datetime
+import urllib
+import requests
+import wechat_config as config
+
+
+def update_token(func):
+    def wrapper(*args, **kwargs):
+        now = datetime.datetime.now()
+        # 刷新token
+        if not config.expires_time or now >= config.expires_time:
+            data = {
+                'grant_type': 'client_credential',
+                'appid': config.appid,
+                'secret': config.appsecret
+            }
+            ret = requests.get(config.token_url + urllib.urlencode(data))
+            result = json.loads(ret.text)
+            config.access_token = result['access_token']
+            config.expires_time = now + datetime.timedelta(hours=2)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@update_token
+def send_msg(openid, title, data):
+    data = json.dumps({
+        'touser': openid,
+        'template_id': config.template_id,
+        'url': config.template_url + title,  # 点击打开的链接
+        'data': {
+            'title': {
+                'value': title,
+                'color': '#173177'
+            },
+            'data': {
+                'value': data,
+                'color': '#173177'
+            },
+        }
+    }).encode()
+    url = config.send_url + config.access_token
+    ret = requests.post(url, data)
+    print 'send result: {}'.format(ret.text)
+
+
+if __name__ == '__main__':
+    from IPython import embed
+    embed()
+    for maintainer in config.maintainers:
+        send_msg(maintainer, 'hello', 'start running~')
